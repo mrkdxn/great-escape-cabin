79507bd7681179915399e980d1ed2a450e8397c5
Mark Dixon <mark.dixon@lightspeedhq.com>
2021-03-20 10:21:33 -0400

    Smaller images, https via JS

--- js/m.js
+++ js/m.js
@@ -1,3 +1,8 @@
+console.log(document.location);
+if (document.location.protocol == 'http:')
+{
+    document.location.href = 'https://'+document.location.pathname;
+}
 
 window.addEventListener('load', function () {
     let fotos = new Photos();
@@ -5,34 +10,35 @@ window.addEventListener('load', function () {
 });
 
 const Photos = function () {
-
 };
-
 
 Photos.prototype = {
 
     flickrData : '',
-
+    timestamp : '',
     init: function()
     {
-        this.flickrAPI();
+        if(localStorage.getItem('flickrData'))
+        {
+            this.buildFlickPage();
+        }
+        else
+        {
+            this.flickrAPI();
+        }
     },
 
     buildPhotoURL: function(photo)
     {
-
         let thePhoto = 'https://live.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret;
         return thePhoto;
     },
     buildFlickrObject: function()
     {
-
         let photos = this.flickrData.photoset.photo;
-
         var myFlickrObject = [];
         for ( image in photos)
         {
-            console.log();
             var obj = {
                 url: this.buildPhotoURL(photos[image]),
                 title: photos[image].title,
@@ -42,6 +48,7 @@ Photos.prototype = {
             myFlickrObject.push(obj);
         }
         localStorage.setItem('flickrData',  JSON.stringify(myFlickrObject));
+        this.setTimestamp();
         this.buildFlickPage();
     },
     buildFlickPage: function ()
@@ -56,20 +63,37 @@ Photos.prototype = {
         {
             this.addPhoto(photos[image]);
         }
-
-        var lightbox = new SimpleLightbox('.photo a', { /* options */ });
+        var lightbox = new SimpleLightbox('.photo a', {  });
+        lazyLoadInstance.update();
+    },
+    setTimestamp: function ()
+    {
+        var d = new Date();
+        var n = d.getTime();
+        localStorage.setItem('flickrTime', n);
+        this.timestamp = n;
     },
     addPhoto: function (photo)
     {
+        // div
         var element = document.createElement("div");
         element.className = 'photo';
+        // img
         var img = document.createElement('img');
-        img.src = photo.url+'_e.jpg';
+        //img.src = photo.url+'_m.jpg';
         img.title = photo.title;
+        img.className = 'lazy';
+        img.setAttribute('data-src', photo.url+'_m.jpg');
         element.appendChild(img);
         document.getElementById('photos').appendChild(element);
-        $(img).wrap("<a href='"+photo.url+'_b.jpg'+"'></a>'");
-
+        // ahref
+        var a = document.createElement('a');
+        a.href = photo.url+'_b.jpg';
+        this.wrap(img, a);
+    },
+    wrap: function (el, wrapper) {
+        el.parentNode.insertBefore(wrapper, el);
+        wrapper.appendChild(el);
     },
     flickrAPI: function ()
     {
@@ -90,9 +114,7 @@ Photos.prototype = {
                 }
             }
         };
-
         xmlhttp.open("GET", "https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=2c45c27714bec652243d7e68e869506e&photoset_id=72157715632321882&user_id=80995589%40N00&extras=tags,description&format=json&nojsoncallback=1", true);
         xmlhttp.send();
-
     }
 }
